45a08ef4da26a6ed482ceba3eeae7257f824ee1a
ADITHYA RAJ <145443565+Adii246@users.noreply.github.com>
2025-01-07 23:33:39 +0530

    Create basics1.js

---
+++ basics1.js
@@ -0,0 +1,48 @@
+var p="jili";
+let b="huju";
+document.write(p);
+document.write("<br>")
+document.write(typeof b);
+document.write("<br>")
+
+var num1=10;
+var n2=52.3
+document.write(num1);
+document.write("<br>")
+document.write(typeof n2);
+document.write("<br>")
+
+var b1=true;
+var hy=false;
+document.write(b1);
+document.write("<br>")
+document.write(typeof hy);
+document.write("<br>")
+
+var test1;
+var t2=null;
+document.write(test1);
+document.write("<br>")
+document.write(typeof t2);
+document.write("<br>");
+
+var y = 10, x = 5;
+document.write(y+x);
+
+const pi=3.14;
+document.write(pi);
+ pi=4.5;
+ document.write(pi);
+
+
+var a=10;
+var b=20;
+document.write(a-b);
+document.write("<br>");
+a++;
+document.write(a+b);
+document.write("<br>");
+document.write(a*b);
+document.write("<br>");
+document.write(b/a);
+document.write("<br>");
